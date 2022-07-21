7cdab4c568e6057d858aed0458d6c170dc849893
DenisHura <hura.dv88@gmail.com>
2022-07-21 19:05:25 +0300

    add files

---
+++ js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+function onCreateGalleryMarkup(array) {
+    return array.map(({ preview, original, description }) => 
+    `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `).join('')
+  
+}
+
+galleryEl.insertAdjacentHTML("beforeend", onCreateGalleryMarkup(galleryItems))
+
+galleryEl.addEventListener("click", onOpenModalOriginalImage)
+galleryEl.addEventListener("keyboard", onOpenModalOriginalImage)
+
+function onOpenModalOriginalImage(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+   const instance = basicLightbox.create(`
+    <img src=${evt.target.dataset.source} width="800" height="600">
+`)
+  instance.show()
+     
+}
+
+
+
+// function onCreateGalleryMarkup(array) {
+
+//     return array.reduce((acc, item) => 
+//         acc + `<div class="gallery__item">
+//   <a class="gallery__link" href="${item.original}">
+//     <img
+//       class="gallery__image"
+//       src="${item.preview}"
+//       data-source="${item.original}"
+//       alt="${item.description}"
+//     />
+//   </a>
+// </div>`, ""
+//     )
+// }
---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+function onCreateGalleryMarkup(array) {
+    return array.reduce((acc, {original, preview, description}) => 
+        acc + `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`, " "
+    )
+}
+
+listEl.insertAdjacentHTML("beforeend", onCreateGalleryMarkup(galleryItems))
+
+
+new SimpleLightbox((".gallery a"), {
+    captionsData: "alt",
+    captionDelay: 250
+
+})

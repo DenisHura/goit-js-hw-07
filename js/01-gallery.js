6e3f567201e6a721afdd186a39437648e30b461e
DenisHura <hura.dv88@gmail.com>
2022-07-22 10:37:45 +0300

    add close gallery by Esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ import { galleryItems } from './gallery-items.js';
 const galleryEl = document.querySelector(".gallery");
 
 function onCreateGalleryMarkup(array) {
-    return array.map(({ preview, original, description }) => 
+  return array.map(({ preview, original, description }) =>
     `
     <div class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -16,27 +16,34 @@ function onCreateGalleryMarkup(array) {
     />
   </a>
 </div>
-    `).join('')
+    `).join('');
   
 }
 
-galleryEl.insertAdjacentHTML("beforeend", onCreateGalleryMarkup(galleryItems))
+galleryEl.insertAdjacentHTML("beforeend", onCreateGalleryMarkup(galleryItems));
 
-galleryEl.addEventListener("click", onOpenModalOriginalImage)
+galleryEl.addEventListener("click", onOpenModalOriginalImage);
 
 
 function onOpenModalOriginalImage(evt) {
   evt.preventDefault();
 
-  if (!evt.target.classList.contains("gallery__image")) {
+  // if (!evt.target.classList.contains("gallery__image")) {
+  if (evt.target.nodeName !== 'IMG') {
     return;
   }
-   const instance = basicLightbox.create(`
-    <img src=${evt.target.dataset.source} width="800" height="600">
-`)
-  instance.show()
-     }
 
+  const instance = basicLightbox.create(`
+    <img src=${evt.target.dataset.source} width="800" height="600">
+`);
+  instance.show();
+
+  addEventListener("keydown", evt =>{
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  })
+   }
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,18 +6,18 @@ import { galleryItems } from './gallery-items.js';
 const listEl = document.querySelector(".gallery");
 
 function onCreateGalleryMarkup(array) {
-    return array.reduce((acc, {original, preview, description}) => 
+    return array.reduce((acc, { original, preview, description }) =>
         acc + `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`, " "
-    )
+    );
 }
 
-listEl.insertAdjacentHTML("beforeend", onCreateGalleryMarkup(galleryItems))
+listEl.insertAdjacentHTML("beforeend", onCreateGalleryMarkup(galleryItems));
 
 
 new SimpleLightbox((".gallery a"), {
     captionsData: "alt",
     captionDelay: 250
 
-})
+});
